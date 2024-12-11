733f86b8bb91239a4a2c6b22987c7eb7fab8414b
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-12-11 23:49:47 +0500

    Task6

--- Tyuiu.AsharabzyanovaAR.Sprint6.Task6.V9.Lib/DataService.cs
+++ Tyuiu.AsharabzyanovaAR.Sprint6.Task6.V9.Lib/DataService.cs
@@ -4,20 +4,20 @@ namespace Tyuiu.AsharabzyanovaAR.Sprint6.Task6.V9.Lib
 {
     public class DataService : ISprint6Task6V9
     {
-        public string CollectTextFromFile(string str,string path)
+        public string CollectTextFromFile(string str, string path)
         {
             string result = "";
             using (StreamReader reader = new StreamReader(path))
             {
                 string? line;
 
-               
+
                 while ((line = reader.ReadLine()) != null)
                 {
-                    
+
                     string[] fragments = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    
+
                     if (fragments.Length > 0)
                     {
                         result += fragments[^1] + " ";
@@ -25,9 +25,7 @@ namespace Tyuiu.AsharabzyanovaAR.Sprint6.Task6.V9.Lib
                 }
             }
             result = result.Trim();
-            return result; 
+            return result;
         }
-
-        
     }
 }
